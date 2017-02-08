5fa9b017f827595f17150b9747418ada056e1a18
Daniil Sydorenko <daniildeveloper@gmail.com>
2017-02-08 01:33:53 +0100

    Fixes in errors codes

---
+++ app/errors/Errors.js
@@ -0,0 +1,22 @@
+"use strict";
+
+class Errors {
+	constructor() {
+		this.E_001 = 'Property is not defined';
+		this.E_002 = 'Invalid type of parameter';
+		this.E_003 = 'Property is empty';
+		this.E_004 = '';
+		this.E_005 = '';
+		this.E_006 = 'Missing container selector property';
+		this.E_007 = 'Missing item selector property';
+		this.E_008 = 'Missing width of item property';
+		this.E_009 = 'Missing margin property';
+		this.E_010 = 'Missing container max width property';
+	}
+
+	throwError(customMessage, errorCode) {
+		throw new Error(customMessage + ": " + this[errorCode]);
+	}
+}
+
+module.exports = new Errors();
